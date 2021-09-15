7f4a7ebdb181ac6e0062c3869ea5358ebdfd0392
takeeggnathan <90749638+takeeggnathan@users.noreply.github.com>
2021-09-15 23:08:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function openFunction() {
+  document.getElementById("menu").style.width = "300px";
+  document.getElementById("mainbox").style.marginLeft = "300px";
+
+}
+function closeFunction() {
+    document.getElementById("menu").style.width = "0px";
+    document.getElementById("mainbox").style.marginLeft = "0px";
+}
+
